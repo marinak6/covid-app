7b06fa4435ea4b65ddc3f2e1891b27728f0addc5
Marina Kun <mk3qa@virginia.edu>
2020-10-23 17:58:02 -0400

    add basic functionality of autocomplete

--- covid-app/components/Search.js
+++ covid-app/components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 import Autocomplete from 'react-native-autocomplete-input';
 
 const countyData = [
@@ -16,7 +16,12 @@ class Search extends React.Component {
         super(props)
         this.state = {
             query: "",
-            countyData: []
+            countyData: [{ fips: 123, text: "Prince William" },
+            { fips: 124, text: "Albemarle" },
+            { fips: 125, text: "Prince george" },
+            { fips: 126, text: "Gainesville" },
+            { fips: 127, text: "Justin Beiber" },
+            { fips: 128, text: "idk" }]
 
         }
     }
@@ -37,25 +42,42 @@ class Search extends React.Component {
         return countyData.filter(county => county.text.search(regex) >= 0);
     }
 
+    onSelect(item) {
+        console.log(item)
+    }
+
+    renderItem = ({ item, i, separators }) => {
+        return (
+            <TouchableOpacity key={i} onPress={() => this.onSelect(item)}>
+                <Text>
+                    {item.text}
+                </Text>
+            </TouchableOpacity>
+        )
+    }
+
     render() {
         const { query } = this.state;
         const counties = this.findCounty(query);
+        console.log(counties)
         const comp = (a, b) => a.toLowerCase().trim() === b.toLowerCase().trim();
-
+        const data = counties.length === 1 && comp(query, counties[0].text) ? [] : counties
+        console.log(data)
         return (
             <View style={styles.screen}>
                 <Text>Search screen</Text>
                 <Autocomplete
                     autoCapitalize="none"
+                    containerStyle={styles.autocompleteContainer}
                     autoCorrect={false}
-                    data={counties.length === 1 && comp(query, counties[0].text) ? [] : counties}
+                    data={data}
                     defaultValue={query}
                     onChangeText={text => this.setState({ query: text })}
                     placeholder="Enter U.S. county"
-                    renderItem={({ item, i }) => (
-                        <TouchableOpacity onPress={() => this.setState({ query: item })}>
+                    renderItem={({ item, i, separators }) => (
+                        <TouchableOpacity key={i} onPress={() => this.onSelect(item)}>
                             <Text>
-                                {item}
+                                {item.text}
                             </Text>
                         </TouchableOpacity>
                     )}
@@ -81,5 +103,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'white',
-    }
+    },
+    autocompleteContainer: {
+        marginLeft: 10,
+        marginRight: 10
+    },
 });
