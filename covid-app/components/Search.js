8b3512a3a9cf8db5c88a4c4c916cc5ff38396305
morgan freiberg <msf6wk@virginia.edu>
2020-11-02 02:17:55 -0500

    updating autocomplete and flushing out some search results (including prediction graphs)

--- covid-app/components/Search.js
+++ covid-app/components/Search.js
@@ -1,28 +1,20 @@
 import React from 'react'
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text, Image, View, Button, StyleSheet, TouchableOpacity } from 'react-native'
 import Autocomplete from 'react-native-autocomplete-input';
 
-const countyData = [
-    { fips: 123, text: "Prince William" },
-    { fips: 124, text: "Albemarle" },
-    { fips: 125, text: "Prince george" },
-    { fips: 126, text: "Gainesville" },
-    { fips: 127, text: "Justin Beiber" },
-    { fips: 128, text: "idk" },
-]
+
+const countyData = require('../fips_codes.json');
+
 class Search extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
             query: "",
-            countyData: [{ fips: 123, text: "Prince William" },
-            { fips: 124, text: "Albemarle" },
-            { fips: 125, text: "Prince george" },
-            { fips: 126, text: "Gainesville" },
-            { fips: 127, text: "Justin Beiber" },
-            { fips: 128, text: "idk" }]
-
+            madeSelection: false,
+            fips: "",
+            display: false,
+            source:""
         }
     }
 
@@ -44,6 +36,13 @@ class Search extends React.Component {
 
     onSelect(item) {
         console.log(item)
+        console.log(item.text)
+        this.setState({
+            query: item.text,
+            madeSelection: true,
+            fips: item.fips
+        })
+        console.log(this.state)
     }
 
     renderItem = ({ item, i, separators }) => {
@@ -56,19 +55,50 @@ class Search extends React.Component {
         )
     }
 
+    returnQuery(){
+        return this.state['query']
+    }
+
+
+    changeSelection(){
+        console.log('reset state')
+        this.setState({
+            query: "",
+            madeSelection: false,
+            display: false,
+            source: ""
+        })
+    }
+
+    displayThings(){
+        const {fips} = this.state
+        const source = require("../prediction_graphs/" + fips + ".png")
+        this.setState({
+            source: source,
+            display: true
+        })
+
+    }
+
     render() {
         const { query } = this.state;
+        const { madeSelection } = this.state;
+        const { source } = this.state;
+        const { display } = this.state;
+        console.log(query)
         const counties = this.findCounty(query);
         console.log(counties)
         const comp = (a, b) => a.toLowerCase().trim() === b.toLowerCase().trim();
         const data = counties.length === 1 && comp(query, counties[0].text) ? [] : counties
+        const { countyData } = this.state;
         console.log(data)
         return (
             <View style={styles.screen}>
-                <Text>Search screen</Text>
-                <Autocomplete
+
+                {!madeSelection && <Autocomplete
                     autoCapitalize="none"
                     containerStyle={styles.autocompleteContainer}
+                    style={{borderColor:'black', borderWidth:1, backgroundColor: 'white',  fontSize:20}}
                     autoCorrect={false}
                     data={data}
                     defaultValue={query}
@@ -76,12 +106,62 @@ class Search extends React.Component {
                     placeholder="Enter U.S. county"
                     renderItem={({ item, i, separators }) => (
                         <TouchableOpacity key={i} onPress={() => this.onSelect(item)}>
-                            <Text>
+                            <Text style={{fontSize:20}}>
                                 {item.text}
                             </Text>
                         </TouchableOpacity>
                     )}
-                />
+                />}
+                {madeSelection && 
+                    <Text style={styles.titleText}> Selected {query} </Text>}
+
+                
+                { madeSelection && <View style={styles.sideBySide}>
+                    <View style={styles.button}>
+                        <Button
+                            onPress={this.changeSelection.bind(this)}
+                            title="Change selection"
+                            color="#841584"
+                            accessibilityLabel="Learn more about this purple button"
+                            />
+                        </View>
+
+                    <View style={styles.button}>
+                        <Button
+                            onPress={this.displayThings.bind(this)}
+                            title="Get info"
+                            color="#841584"
+                            accessibilityLabel="Learn more about this purple button"
+                            />
+                    </View>
+                </View>}
+                    
+                    
+                {display && 
+                <View style={{margin:10, padding:0}}>
+                    
+                    <Text style={styles.titleText}> {query} Cummulative Cases Predictions</Text>
+
+                    <View style={styles.sideBySide}>
+                        <Image
+                            style={styles.stretch}
+                            source={source}
+                            />
+                        <View style={styles.statsBox}>
+                            <Text style={styles.titleText}> maybe put some stats here</Text>
+                        </View>
+                    </View>
+
+                    <Text style={styles.titleText}> {query} Twitter Sentiment</Text>
+                    <View style={styles.sideBySide}>
+                        <View style={styles.negative}><Text style={styles.titleText}>Negative</Text></View>
+                        <View style={styles.neutral}><Text style={styles.titleText}>Neutral</Text></View>
+                        <View style={styles.positive}><Text style={styles.titleText}>Positive</Text></View>
+
+                    </View>
+    
+                </View>   
+                }             
             </View>
         )
     }
@@ -89,11 +169,47 @@ class Search extends React.Component {
 export default Search
 
 const styles = StyleSheet.create({
+    titleText: {
+        margin:10,
+        fontSize: 20,
+        fontWeight: "bold"
+      },
+    sideBySide: {
+        flex: 1,
+        padding:0,
+        flexDirection: 'row'
+        // justifyContent: 'left' //'space-between'
+    },
+    statsBox:{
+        height: 500,
+        width: 400,
+        borderColor: "grey",
+        borderWidth: 5,
+        margin: 10
+    },
+    center:{
+        flex: 1, 
+        alignItems: 'center',
+        justifyContent: 'center', 
+        margin:10,
+    },
+    button:{
+        margin:10,
+        width: "30%",
+        padding:0
+    },
+    stretch: {
+        width: 1400,
+        height: 500,
+        margin:10,
+        resizeMode: 'stretch',
+    },
     screen: {
         flex: 1,
         height: "100%",
         width: "100%",
         flexDirection: 'column',
+        margin:10
 
     },
     container: {
@@ -101,11 +217,50 @@ const styles = StyleSheet.create({
         height: "100%",
         width: "100%",
         justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white',
+        alignItems: 'center'
     },
     autocompleteContainer: {
-        marginLeft: 10,
-        marginRight: 10
+        marginLeft: 20,
+        marginRight: 500,
+        fontSize: 20
     },
+    negative: {
+        margin:10,
+        fontSize: 20,
+        width:200,
+        height:200,
+        fontWeight: "bold",
+        borderColor: "red",
+        borderWidth: 5,
+        backgroundColor: "#ffcccb",
+        flex: 1, 
+        alignItems: 'center',
+        justifyContent: 'center', 
+      },
+      neutral: {
+        margin:10,
+        fontSize: 20,
+        width:200,
+        height:200,
+        fontWeight: "bold",
+        borderColor: "grey",
+        borderWidth: 5,
+        backgroundColor: "#d7d7d7",
+        flex: 1, 
+        alignItems: 'center',
+        justifyContent: 'center', 
+      },
+      positive: {
+        margin:10,
+        fontSize: 20,
+        width:200,
+        height:200,
+        fontWeight: "bold",
+        borderColor: "green",
+        borderWidth: 5,
+        backgroundColor: "#b2ff59",
+        flex: 1, 
+        alignItems: 'center',
+        justifyContent: 'center', 
+      },  
 });
