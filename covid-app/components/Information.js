80c8998a69727ca13cb54c3a118e8465ac08b088
Marina Kun <mk3qa@virginia.edu>
2020-11-04 13:27:29 -0500

    Styled information

--- covid-app/components/Information.js
+++ covid-app/components/Information.js
@@ -7,49 +7,50 @@ class Information extends React.Component {
     constructor(props) {
         super(props)
     }
-
     render() {
         return (
             <View style={styles.screen}>
-                <Text style={styles.titleText}>FAQ</Text>
-                <View style={styles.section}>
-                    <Text style={styles.infoTitle}>Infections Model</Text>
-                    <View style={styles.faq}>
-                        <Text style={styles.question}> What's the accuracy for the model? </Text>
-                        <Paragraph> 87% </Paragraph>
+                <View style={styles.faqsection}>
+                    <Text style={styles.titleText}>FAQ</Text>
+                    <View style={styles.section}>
+                        <Text style={styles.infoTitle}>Infections Model</Text>
+                        <View style={styles.faq}>
+                            <Text style={styles.question}> What's the accuracy for the model? </Text>
+                            <Paragraph>     • 87% </Paragraph>
+                        </View>
+                        <View style={styles.faq}>
+                            <Text style={styles.question}> What model are you using? </Text>
+                            <Paragraph>     • ARIMA </Paragraph>
+                        </View>
+                        <View style={styles.faq}>
+                            <Text style={styles.question}> What's a confidence interval? </Text>
+                            <Paragraph>     • </Paragraph>
+                        </View>
+                        <View style={styles.faq}>
+                            <Text style={styles.question}> Where did you get your data for COVID-19 infections</Text>
+                            <Paragraph>     • </Paragraph>
+                        </View>
                     </View>
-                    <View style={styles.faq}>
-                        <Text style={styles.question}> What model are you using? </Text>
-                        <Paragraph> ARIMA </Paragraph>
-                    </View>
-                    <View style={styles.faq}>
-                        <Text style={styles.question}> What's a confidence interval? </Text>
-                        <Paragraph> </Paragraph>
-                    </View>
-                    <View style={styles.faq}>
-                        <Text style={styles.question}> Where did you get your data for COVID-19 infections</Text>
-                        <Paragraph> </Paragraph>
-                    </View>
-                </View>
-                <View style={styles.section}>
-                    <Text style={styles.infoTitle}>Twitter Analysis</Text>
-                    <View style={styles.faq}>
-                        <Text style={styles.question}> What's the accuracy for the model? </Text>
-                        <Paragraph> 89%</Paragraph>
-                    </View>
-                    <View style={styles.faq}>
-                        <Text style={styles.question}> What model are you using? </Text>
-                        <Paragraph> Logistic Regression</Paragraph>
-                    </View>
-                    <View style={styles.faq}>
-                        <Text style={styles.question}> How did you get the tweets?</Text>
-                        <Paragraph> </Paragraph>
-                    </View>
-                    <View style={styles.faq}>
-                        <Text style={styles.question}> Is the tweet sentiment analysis indicative of the area selected? </Text>
-                        <Paragraph> </Paragraph>
-                    </View>
+                    <View style={styles.section}>
+                        <Text style={styles.infoTitle}>Twitter Analysis</Text>
+                        <View style={styles.faq}>
+                            <Text style={styles.question}> What's the accuracy for the model? </Text>
+                            <Paragraph>     • 89%</Paragraph>
+                        </View>
+                        <View style={styles.faq}>
+                            <Text style={styles.question}> What model are you using? </Text>
+                            <Paragraph>     • Logistic Regression</Paragraph>
+                        </View>
+                        <View style={styles.faq}>
+                            <Text style={styles.question}> How did you get the tweets?</Text>
+                            <Paragraph>     • </Paragraph>
+                        </View>
+                        <View style={styles.faq}>
+                            <Text style={styles.question}> Is the tweet sentiment analysis indicative of the area selected? </Text>
+                            <Paragraph>     • </Paragraph>
+                        </View>
 
+                    </View>
                 </View>
             </View>
         )
@@ -68,18 +69,38 @@ const styles = StyleSheet.create({
         alignItems: 'center',
 
     },
+    faqsection: {
+        display: 'flex',
+        width: "60%",
+        height: "100%",
+        flexDirection: 'column',
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     section: {
-
+        display: 'flex',
+        width: "50%",
+        flexDirection: 'column',
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'start',
+        marginBottom: '3%'
     },
     titleText: {
         fontFamily: 'Avenir',
-        fontSize: 50,
+        fontSize: 60,
         fontWeight: 'bold',
+        alignSelf: 'center',
+        marginBottom: '2%',
+        marginTop: "3%"
     },
     infoTitle: {
         fontFamily: 'Avenir',
-        fontSize: 27,
-        color: "#3aba8f"
+        fontSize: 30,
+        color: "#3aba8f",
+        alignSelf: 'center',
+        marginBottom: '3%'
     },
     container: {
         flex: 1,
@@ -93,6 +114,7 @@ const styles = StyleSheet.create({
 
     },
     faq: {
-
+        display: 'flex',
+        marginTop: "2%"
     }
 });
