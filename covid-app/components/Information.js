0309a73b227f3b16f2de4d69463e904c8a16de85
Marina Kun <mk3qa@virginia.edu>
2020-11-07 15:52:34 -0500

    margin left info page

--- covid-app/components/Information.js
+++ covid-app/components/Information.js
@@ -16,22 +16,19 @@ class Information extends React.Component {
                         <Text style={styles.infoTitle}>Infections Model</Text>
                         <View style={styles.faq}>
                             <Text style={styles.question}> What's the accuracy for the model? </Text>
-                            <Paragraph>     • Each county has it's own model. The average accuracy is 87%. </Paragraph>
+                            <Paragraph style={styles.answer}>• Each county has it's own model. The average accuracy is 87%. </Paragraph>
                         </View>
                         <View style={styles.faq}>
                             <Text style={styles.question}> What model are you using? </Text>
-                            <Paragraph>     • We are using Auto Regressive Integrated Moving Average (ARIMA).</Paragraph>
-                            <Paragraph>     • This is a statistical model that makes predictions based solely on previous values.</Paragraph>
-                            <Paragraph>     • Learn more about it <Text style={styles.link} onPress={() => Linking.openURL("https://www.machinelearningplus.com/time-series/arima-model-time-series-forecasting-python/#:~:text=ARIMA%2C%20short%20for%20'Auto%20Regressive,used%20to%20forecast%20future%20values")}>here</Text>!</Paragraph>
+                            <Paragraph style={styles.answer}>• We are using Auto Regressive Integrated Moving Average (ARIMA). This is a statistical model that makes predictions based solely on previous values. Learn more about it <Text style={styles.link} onPress={() => Linking.openURL("https://www.machinelearningplus.com/time-series/arima-model-time-series-forecasting-python/#:~:text=ARIMA%2C%20short%20for%20'Auto%20Regressive,used%20to%20forecast%20future%20values")}>here</Text>!</Paragraph>
                         </View>
                         <View style={styles.faq}>
                             <Text style={styles.question}> What's a confidence interval? </Text>
-                            <Paragraph>     • Our graphs show a 95% confidence interval. Essentially, the true value of the number of total COVID-19 cases will fall within that range 95% of the time.</Paragraph>
+                            <Paragraph style={styles.answer}>• Our graphs show a 95% confidence interval. Essentially, the true value of the number of total COVID-19 cases will fall within that range 95% of the time.</Paragraph>
                         </View>
                         <View style={styles.faq}>
                             <Text style={styles.question}> Where did you get your data for COVID-19 infections</Text>
-                            <Paragraph>     • We are using the New York Times COVID-19 dataset.</Paragraph>
-                            <Paragraph>     • Check it out <Text style={styles.link} onPress={() => Linking.openURL("https://github.com/nytimes/covid-19-data")}>here</Text>!</Paragraph>
+                            <Paragraph style={styles.answer}>• We are using the New York Times COVID-19 dataset. Check it out <Text style={styles.link} onPress={() => Linking.openURL("https://github.com/nytimes/covid-19-data")}>here</Text>!</Paragraph>
 
                         </View>
                     </View>
@@ -39,22 +36,20 @@ class Information extends React.Component {
                         <Text style={styles.infoTitle}>Twitter Analysis</Text>
                         <View style={styles.faq}>
                             <Text style={styles.question}> What's the accuracy for the model? </Text>
-                            <Paragraph>     • The model has a validation accuracy of 89%</Paragraph>
+                            <Paragraph style={styles.answer}>• The model has a validation accuracy of 89%</Paragraph>
                         </View>
                         <View style={styles.faq}>
                             <Text style={styles.question}> What model are you using? </Text>
-                            <Paragraph>     • Logistic Regression</Paragraph>
+                            <Paragraph style={styles.answer}>• Logistic Regression</Paragraph>
                         </View>
                         <View style={styles.faq}>
                             <Text style={styles.question}> How did you get the tweets?</Text>
-                            <Paragraph>     • The tweets used to train our model were collected through the Twitter API based on a collection of hashtags we determined would be indicative of a tweet's stance on supporting or not supporting guidelines.</Paragraph>
-                            <Paragraph>     • The tweets used to create the statistics seen on the search page are based on geo-tagged, COVID-19 related tweet IDs collected by Rabindra Lamsal, a researcher at the School of Computer and Systems Sciences, JNU, New Delhi. These tweet ids were then rehydrated with the Twitter API.</Paragraph>
-                            <Paragraph>     • The tweet ids can be found <Text style={styles.link} onPress={() => Linking.openURL("https://ieee-dataport.org/open-access/coronavirus-covid-19-geo-tagged-tweets-dataset")}>here</Text>!</Paragraph>
+                            <Paragraph style={styles.answer}>• The tweets used to train our model were collected through the Twitter API based on a collection of hashtags we determined would be indicative of a tweet's stance on supporting or not supporting guidelines. The tweets used to create the statistics seen on the search page are based on geo-tagged, COVID-19 related tweet IDs collected by Rabindra Lamsal, a researcher at the School of Computer and Systems Sciences, JNU, New Delhi. These tweet ids were then rehydrated with the Twitter API. The tweet ids can be found <Text style={styles.link} onPress={() => Linking.openURL("https://ieee-dataport.org/open-access/coronavirus-covid-19-geo-tagged-tweets-dataset")}>here</Text>!</Paragraph>
 
                         </View>
                         <View style={styles.faq}>
                             <Text style={styles.question}> Is the tweet sentiment analysis indicative of the area selected? </Text>
-                            <Paragraph>     • The results of the tweet analysis cannot be applied to the county in general. This is because only tweets were considered, which is not a true representative sample of the population.</Paragraph>
+                            <Paragraph style={styles.answer}>• The results of the tweet analysis cannot be applied to the county in general. This is because only tweets were considered, which is not a true representative sample of the population.</Paragraph>
                         </View>
 
                     </View>
@@ -84,6 +79,9 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    answer: {
+        marginLeft: 25,
     },
     section: {
         display: 'flex',
